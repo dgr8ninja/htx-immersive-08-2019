0abbc4d33b8180acc0d6491b5f3e2a163dab08d1
dgr8ninja <dr4602@gmail.com>
2019-09-16 10:57:27 -0500

    Second commit for 091619

--- 03-week/4-friday/labs/NewSite/location.js
+++ 03-week/4-friday/labs/NewSite/location.js
@@ -1,13 +1,9 @@
-var map = new GMaps({
-    el: '#map',
-    lat:  40.730610,
-    lng: -73.935242
-});
+var mymap = L.map('mapid').setView([29.7516754, -95.357813], 15);
+var marker = L.marker([29.7516754, -95.357813]).addTo(mymap);
 
-map.addMarker({
-    lat: 40.700610,
-    lng: -73.997242,
-    title: 'New York'
-});
-
-map.setZoom(8);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1IjoiZGdyOG5pbmphIiwiYSI6ImNrMG1rZzc4dzE4M3kzbHFkOG1oMmJxbDEifQ.Hhadn5czac5nMEuj6gC_Lg'
+}).addTo(mymap);
--- 03-week/4-friday/labs/NewSite/location.php
+++ 03-week/4-friday/labs/NewSite/location.php
@@ -9,7 +9,9 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
+    <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+    integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+    crossorigin=""></script> 
     <script src="http://maps.google.com/maps/api/js?key=YOUR_API_KEY"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.25/gmaps.js"></script>
     
@@ -21,11 +23,10 @@
     integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
     crossorigin=""/>
     <link rel="stylesheet" href="navigationcss.css">
+    <link rel="" href="location.js">
 
     <!-- Make sure you put this AFTER Leaflet's CSS -->
-    <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
-    integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
-    crossorigin=""></script>
+   
 
     <title>Huynh's Restaurant</title>
 </head>
@@ -34,9 +35,8 @@
     <div class="map-example">
     <div class="row">
         <div class="col-lg-6">
-            <div id="map">
-            <img src="map.png">
-            </div>
+        <div id="mapid"></div>
+
         </div>
         <div class="col-lg-6">
             <div class="heading">
@@ -72,6 +72,6 @@
 </div>
 </body>
 
-<script> src="./location.js"</script>
+<script src="./location.js"> </script>
 
 </html>
